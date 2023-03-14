4bfb3918e0707c051ba867faae968557c5f47c06
moruch4nn <moruch4nn@gmail.com>
2023-03-14 13:31:30 +0900

    🐛正しくIPアドレスが保存されないバグを修正

--- src/main/kotlin/dev/mr3n/rtsproxy/RTSProxy.kt
+++ src/main/kotlin/dev/mr3n/rtsproxy/RTSProxy.kt
@@ -92,7 +92,7 @@ class RTSProxy @Inject constructor(private val server: ProxyServer, @DataDirecto
         val request = mapOf(
             "uuid" to event.player.uniqueId.toString(),
             "name" to event.player.username,
-            "ip" to event.player.remoteAddress.hostName,
+            "ip" to event.player.remoteAddress.address.hostAddress,
             "verified" to true
         )
         if(this.users.containsKey(event.player.uniqueId)) {
@@ -121,7 +121,7 @@ class RTSProxy @Inject constructor(private val server: ProxyServer, @DataDirecto
             .append(Component.text("公式Discord鯖の", Style.style(TextColor.color(170, 170, 170))))
             .append(Component.text("#アカウント連携", Style.style(TextColor.color(153, 170, 181))))
             .append(
-                Component.text("(bit.ly/a-rts)", Style.style(TextColor.color(170, 170, 170), TextDecoration.UNDERLINED)).clickEvent(
+                Component.text("(https://bit.ly/a-rts)", Style.style(TextColor.color(170, 170, 170), TextDecoration.UNDERLINED)).clickEvent(
                 ClickEvent.clickEvent(ClickEvent.Action.OPEN_URL, "https://bit.ly/a-rts")))
             .append(Component.text("にアクセス\n", Style.style(TextColor.color(170, 170, 170))))
             .append(Component.text("2.", Style.style(TextColor.color(232, 60, 60))))
